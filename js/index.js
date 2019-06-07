1c47b1d980b722636c66856442ae0fa0cb504fff
Naba <11944066+Naba0123@users.noreply.github.com>
2019-06-07 23:57:17 +0900

    add index plan

--- js/index.js
+++ js/index.js
@@ -1,4 +1,15 @@
 $(function() {
     console.log("index document ready!");
+
+    // prepare of preloading
+
+    // start preload
+
+    // update preload progress bar
+
+    // preload complete
+
+    // open index page
+
 });
 
